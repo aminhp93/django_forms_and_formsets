a57b40516a757e63e7c6b02ec7450beec44bba02
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-15 09:20:15 -0800

    8 - Collecting Data via HTML Form

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,6 +5,12 @@ from .forms import TestForm
 # Create your views here.
 def home(request):
 	form = TestForm()
+	if request.method == "POST":
+		print(request.POST)
+		print(request.POST.get("search"))
+	elif request.method == "GET":
+		print(request.GET)
+
 	template = "form.html"
 	context = {
 		"form": form
