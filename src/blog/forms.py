be3ce039569681f421ec4f386eb459f9d6279cb1
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-15 09:35:07 -0800

    10 - Basic Form Validation

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -5,4 +5,16 @@ class TestForm(forms.Form):
 	boolean = forms.BooleanField()
 	integer = forms.IntegerField()
 	email = forms.EmailField()
+
+	def clean_integer(self, *args, **kargs):
+		integer = self.cleaned_data.get("integer")
+		if integer < 10:
+			raise forms.ValidationError("The interger must be greater than 10")
+		return integer
+
+	def clean_some_text(self, *args, **kwargs):
+		some_text = self.cleaned_data.get("some_text")
+		if len(some_text) < 10:
+			raise forms.ValidationError("At least 10 character")
+		return some_text
 	
