695d1c03d46358d929f91924c6b9e7bd49d25cd9
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-15 09:28:19 -0800

    9 - Collecting Data via Django Form

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,12 +4,17 @@ from .forms import TestForm
 
 # Create your views here.
 def home(request):
-	form = TestForm()
-	if request.method == "POST":
-		print(request.POST)
-		print(request.POST.get("search"))
-	elif request.method == "GET":
-		print(request.GET)
+	form = TestForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+
+
+	# form = TestForm()
+	# if request.method == "POST":
+	# 	print(request.POST)
+	# 	print(request.POST.get("search"))
+	# elif request.method == "GET":
+	# 	print(request.GET)
 
 	template = "form.html"
 	context = {
